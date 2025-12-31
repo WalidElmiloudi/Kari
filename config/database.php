6ecda88bceb6bf33232628890365c26e8804825b
WalidElmiloudi <elwalid38@gmail.com>
2025-12-31 17:23:45 +0100

    creating the Database class

--- config/database.php
+++ config/database.php
@@ -4,7 +4,8 @@ return[
   'host'     => 'localhost',
   'port'     => 3306,
   'user'     => 'root',
-  'password' => ''
+  'password' => '',
+  'dbname'   => 'Kari'
 ]
 
 ?>
---
+++ core/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+final class Database
+{
+    private static ?PDO $instance = null;
+
+    private function __construct(){}
+
+    public static function getInstance()
+    {
+        if(self::$instance === null){
+            $config = require __DIR__.'/../config/database.php';
+            try{
+                self::$instance = new PDO("mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}",
+                                          "{$config['user']}",
+                                          "{$config['password']}",
+                                          [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            }catch(PDOException $error){
+                echo "failed to connect to database".$error->getMessage();
+            }
+        }
+        return self::$instance;
+    }
+}
